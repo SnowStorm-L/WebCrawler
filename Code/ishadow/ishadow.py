304d50b3385a8c7dc6b7a83ee50de9055e09faab
L <L862608263@163.com>
2018-07-15 20:18:56 +0800

    update_ishadow_demo

---
+++ Code/ishadow/ishadow.py
@@ -0,0 +1,68 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/14 PM6:18
+# @Author  : L
+# @Email   : L862608263@163.com
+# @File    : ishadow.py
+# @Software: PyCharm
+
+import re
+import urllib.request
+import urllib.response
+from biplist import *
+
+
+class AutoChangeServer:
+    url = "https://my.ishadowx.net/#"
+    # 桌面复制的配置文件路径,在这个路劲的文件读取 把代码写完成在换正式路径
+    # 否则可能损坏配置文件
+    preference_plist_path = "/Users/l/Desktop/com.jumboapps.shadowsocksx.plist"
+
+    # 这个是配置文件路径
+    # "/Users/l/Library/Preferences/com.jumboapps.shadowsocksx.plist"
+
+    def open_preference_plist(self):
+        try:
+            plist = readPlist(self.preference_plist_path)
+            # print(plist)
+        except Exception as e:
+            print("Not a plist:", e)
+
+        relation_dict = plist
+        server_list = relation_dict["key.server.list"]
+        for each_line in server_list:
+            print(each_line[:8])
+            # 加密过的data类型数据,(旧版的是json编码成的base64)
+            # 现在还没破解编码方式
+            value = each_line[8: 12]
+            print(value.decode())
+            print(type(value))
+
+    def run(self):
+        response_html = self.url_open(self.url).decode('utf-8')
+
+        ip_list = re.findall('IP Address:<span id="ip[a-z]{3,4}">(.*?)</span>', response_html)
+
+        print(ip_list)
+
+        port_list = re.findall('Port:<span id="port[a-z]{3,4}">([0-9]{5})', response_html)
+
+        print(port_list)
+
+        password_list = re.findall('<span id="pw[a-z]{3,4}">([a-z.\-0-9]{1,17})', response_html)
+
+        print(password_list)
+
+    @staticmethod
+    def url_open(url, data=None):
+        request = urllib.request.Request(url)
+        request.add_header('User-Agent',
+                           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+                           'Version/11.1 Safari/605.1.15')
+        response = urllib.request.urlopen(request, data)
+        return response.read()
+
+
+if __name__ == "__main__":
+    auto_change_server = AutoChangeServer()
+    auto_change_server.open_preference_plist()
