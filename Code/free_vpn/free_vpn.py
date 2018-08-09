d9c6d1a12366e136536d08034d789e3e4de965dc
L <L862608263@163.com>
2018-08-10 01:08:46 +0800

    update

--- Code/free_vpn/free_vpn.py
+++ Code/free_vpn/free_vpn.py
@@ -26,9 +26,64 @@ def url_open(url, data=None):
         print(e.reason)
 
 
+# NOTE Base64解码
+
+# 在解码前，如果字符串中有包含 – 和 _ 的字符，要先分别替换为 + 和 /
+
+# NOTE ss 链接构成
+
+# 在 Base64 编码之前，ss链接的格式是这样的：
+# ss://method:password@server:port
+# 也就是说，一般我们见到的链接就是 ss://Base64编码字段
+# 其中 method:password@server:port 这部分被进行了 Base64 编码
+
+# demo
+# 例如有一个这样的ss链接 ss://Y2hhY2hhMjA6ZG91Yi5pby9zc3poZngvKmRvdWIuYmlkL3NzemhmeC8qMjk4N0A2NC4xMzcuMjI5LjE1NDoyOTg3
+
+# 那么以下的这部分字符串就是经过 Base64 编码生成的：
+# Y2hhY2hhMjA6ZG91Yi5pby9zc3poZngvKmRvdWIuYmlkL3NzemhmeC8qMjk4N0A2NC4xMzcuMjI5LjE1NDoyOTg3
+
+# 上面的字符串，base64解码出来就是（相对应的就是加密方法、密码、ip、端口:(以:或@分割)
+# chacha20 : doub.io/sszhfx/*doub.bid/sszhfx/*2987 @ 64.137.229.154 : 2987
+
+# NOTE ssr 链接构成
+
+# 在 Base64 编码之前，ssr链接的格式是这样的：
+# ssr://server:port:protocol:method:obfs:password_base64/?params_base64
+
+# demo
+# ssr://NjQuMTM3LjIwMS4yNDY6Mjk4NzphdXRoX3NoYTFfdjQ6Y2hhY2hhMjA6dGxzMS4yX3RpY2tldF9hdXRoOlpHOTFZaTVwYnk5emMzcG9abmd2S21SdmRXSXVZbWxrTDNOemVtaG1lQzhxTWprNE53Lz9yZW1hcmtzPTVweXM2TFNtNVktMzVwMmw2SWVxT21SdmRXSXVhVzh2YzNONmFHWjRMLW1Wbk9XRGotV2ZuLVdRalRwa2IzVmlMbUpwWkM5emMzcG9abmd2
+
+# 没有需要替换的(_和-符号), 直接base64解码, 得到如下字符串
+
+# 其中这些都已经很明显了 server:port:protocol:method:obfs:   (obfs: 混淆模式)
+
+# 64.137.201.246:2987:auth_sha1_v4:chacha20:tls1.2_ticket_auth:ZG91Yi5pby9zc3poZngvKmRvdWIuYmlkL3NzemhmeC8qMjk4Nw/?remarks=5pys6LSm5Y-35p2l6IeqOmRvdWIuaW8vc3N6aGZ4L-mVnOWDj-Wfn-WQjTpkb3ViLmJpZC9zc3poZngv
+
+# 混淆模式之后就是密码了  (/?之前, obfs: 之后的这段)
+# ZG91Yi5pby9zc3poZngvKmRvdWIuYmlkL3NzemhmeC8qMjk4Nw base64解码后得到 doub.io/sszhfx/*doub.bid/sszhfx/*2987
+
+# params_base64 是协议参数、混淆参数、备注及Group对应的参数值被 base64编码 后拼接而成的字符串。
+# 即 params_base64为这些字段的拼接：
+# obfsparam=obfsparam_base64&protoparam=protoparam_base64&remarks=remarks_base64&group=group_base64
+
+# 因为该字段只包含 remarks ，说明其余参数都为空
+# 先把remarks不符合规则的 - 字符替换为 +
+# 5pys6LSm5Y-35p2l6IeqOmRvdWIuaW8vc3N6aGZ4L-mVnOWDj-Wfn-WQjTpkb3ViLmJpZC9zc3poZngv
+
+# base64_remarks 字符串如下
+# 5pys6LSm5Y+35p2l6IeqOmRvdWIuaW8vc3N6aGZ4L+mVnOWDj+Wfn+WQjTpkb3ViLmJpZC9zc3poZngv
+
+# base64解码后的内容如下
+# 本账号来自:doub.io/sszhfx/镜像域名:doub.bid/sszhfx/
+
+# 以上内容, 参考文章 https://coderschool.cn/2498.html
+
 class Ishadowx:
     # 2018,8,8 这个网址被封了
     __url = "https://my.ishadowx.net/#"
+
+    # 我用的是macbook, 所以替换方法是接下来的处理
     # 桌面复制的配置文件路径,在这个路径的文件读取 把代码写完成在换正式路径
     # 否则可能损坏配置文件
     __preference_plist_path = "/Users/l/Desktop/com.jumboapps.shadowsocksx.plist"
@@ -37,11 +92,8 @@ class Ishadowx:
     # "/Users/l/Library/Preferences/com.jumboapps.shadowsocksx.plist"
 
     def open_preference_plist(self):
-        try:
-            plist = readPlist(self.__preference_plist_path)
-            # print(plist)
-        except Exception as e:
-            print("Not a plist:", e)
+
+        plist = readPlist(self.__preference_plist_path)
 
         server_list = plist["key.server.list"]
         for each_line in server_list:
@@ -81,10 +133,9 @@ class Blog:
                 detail_info = url_open(detail_url)
                 re_ss_list = re.findall('ss://(.*?)</p>', detail_info)
                 re_ssr_list = re.findall('ssr://(.*?)</p>', detail_info)
-                # print(re_ssr_list)
 
-                ss_list = self.remake_ss_list(re_ss_list)
-                print(ss_list)
+                self.remake_ssr_list(re_ssr_list)
+                self.remake_ss_list(re_ss_list)
 
     def remake_ss_list(self, re_ss_list):
         decode_list = [self.decode_base64(x) for x in re_ss_list if self.decode_base64(x) is not None]
@@ -92,23 +143,34 @@ class Blog:
         key_list = ["method", "password", "server", "server_port"]
         value_list = list(map(lambda x: x.split(), sub_list))
         remake_result = [{key: value for key, value in zip(key_list, x)} for x in value_list]
-        return remake_result
+        print(remake_result)
+
+    def remake_ssr_list(self, re_ssr_list):
+        decode_list = [self.decode_base64(x) for x in re_ssr_list if self.decode_base64(x) is not None]
+        # /?后面都是些辅助参数, 例如混淆参数这些, 对实际获取连接ssr没太大作用
+        # 如果非要解析出来, 可以按照ssr的构造说明去解析
+        split_list = [x[:str(x).find("/?")] for x in decode_list]
+        sub_list = [re.sub("[:]", " ", x) for x in split_list]
+        value_list_with_encode_password = list(map(lambda x: x.split(), sub_list))
+        decode_password_list = list(map(lambda x: self.decode_base64(x[-1]), value_list_with_encode_password))
+        value_list = [x[:-1] for x in value_list_with_encode_password]
+        key_list = ["server", "port", "protocol", "method", "obfs", "password"]
+        for idx, element in enumerate(value_list):
+            element.append(decode_password_list[idx])
+        remake_result = [{key: value for key, value in zip(key_list, x)} for x in value_list]
+        print(remake_result)
 
     @staticmethod
     def decode_base64(data):
-        """Decode base64, padding being optional.
-
-        :param data: Base64 data as an ASCII byte string
-        :returns: The decoded byte string.
-
-        """
+        origin_data = data
         missing_padding = 4 - len(data) % 4
         if missing_padding:
             data += '=' * missing_padding
         try:
+            data = str(data).replace("–", "+").replace("_", "/")
             return base64.b64decode(data).decode('utf-8').replace("\n", "")
         except Exception as e:
-            print(e)
+            print('解密: %s 失败 \n失败原因: %s\n原始数据: %s' % (data, e, origin_data))
             return None
 
 
@@ -117,12 +179,8 @@ if __name__ == "__main__":
     # ishadow = Ishadowx()
     # ishadow.run()
 
-    # 某人博客的
-    blog = Blog()
-    blog.fetch_page()
-    #
-    # tt = "aes-256-cfb:isx.yt-05379585@c.isxc.top:17568"
-    # jj =
-    # print()
+    # # 某人博客的
+    # blog = Blog()
+    # blog.fetch_page()
 
-    # print([x.split(":") for x in str.split("@")])
+    pass
